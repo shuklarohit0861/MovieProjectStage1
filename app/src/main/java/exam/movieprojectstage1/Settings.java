595cecdfc37fadc52cccab559435a08597e18e73
Rohit Shukla <shuklarohit086@gmail.com>
2016-07-29 07:39:48 +0530

    finished the stage 1 but late to the party

---
+++ app/src/main/java/exam/movieprojectstage1/Settings.java
@@ -0,0 +1,55 @@
+package exam.movieprojectstage1;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Rohit Shukla on 28-07-2016.
+ */
+public class Settings extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.prefe_movies);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_key_value)));
+
+    }
+        //@copied from sunshine app setting preference
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    //copied form sunshine app
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object o) {
+        String stringValue = o.toString();
+
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
+}
